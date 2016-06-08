6871188d52dff453f7266aba4e5d4b98ff76fe57
Joni Collinge <jonathan-collinge@hotmail.com>
2016-06-08 08:20:53 +0100

    Added classroom data members

--- Classroom/Classroom.cs
+++ Classroom/Classroom.cs
@@ -7,15 +7,18 @@ using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
-using Classroom.Interfaces;
 
 namespace Classroom
 {
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class Classroom : StatefulService, IClassroom
+    internal sealed class Classroom : StatefulService
     {
+        private Presenter _presenter;
+        private int _steps;
+        private int _pin;
+
         public Classroom(StatefulServiceContext context)
             : base(context)
         { }
