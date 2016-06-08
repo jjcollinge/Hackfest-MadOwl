f50a798d01b9849832d80cfba9fc03534caaf3f0
Stijn Michiels <stmich@microsoft.com>
2016-06-08 15:23:11 +0200

    nearly there

--- MadOwlClient/app/scripts/main.js
+++ MadOwlClient/app/scripts/main.js
@@ -1,3 +1,5 @@
+var dataurl = "dummy.json";
+//var dataurl = "http://madowlkeyvault.westeurope.cloudapp.azure.com:8786/api/classroom?classname=classroom1"
 
 console.log("hwhoo hwhoo");
 
@@ -7,14 +9,16 @@ document.getElementById("btn-join").addEventListener("click", function(){
     var pin = document.getElementById('pin').value;
     var username = document.getElementById('username').value;
     
-    $.getJSON( "dummy.json", function( data ) {
+    $.getJSON( dataurl, function( data ) {
+    
+    console.log(data);
 
         if(pin == data.PinCode){
             $('#notice-success').show();
             setTimeout(function() { 
                 $('#notice-success').fadeOut(); 
             }, 5000);
-            window.location.replace("userapp.html?pin="+encodeURIComponent(data.PinCode)+"&name="+encodeURIComponent(username)+"&hol="+encodeURIComponent(data.Name)+"&steps="+encodeURIComponent(data.StepCount));
+            window.location.replace("userapp.html?pin="+encodeURIComponent(data.Id)+"&name="+encodeURIComponent(username)+"&hol="+encodeURIComponent('Azure Hands-on-Lab')+"&steps="+encodeURIComponent(data.NumSteps));
         } else {
             $('#notice-fail').show();
             setTimeout(function() { 
