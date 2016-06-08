170103821038987566b0bc5d066f1bb36a030265
Jamie D <daltskin@hotmail.com>
2016-06-08 12:15:16 +0100

    Added publish settings

---
+++ ClassroomManagementARM/ClassroomManagementARM/Scripts/Secure Connection to Service Cluster.ps1
@@ -0,0 +1,9 @@
+﻿#Import-PfxCertificate -Exportable -CertStoreLocation Cert:\CurrentUser\My `
+#        -FilePath C:\@SourceControl\Github\MadOwl\ClassroomManagementARM\ClassroomManagementARM\Certs\madowlcert.pfx `
+#        -Password (ConvertTo-SecureString -String abcd123 -AsPlainText -Force)
+
+Connect-serviceFabricCluster -ConnectionEndpoint madowlkeyvault.westeurope.cloudapp.azure.com:19000 `
+    -KeepAliveIntervalInSec 10 `
+    -X509Credential -ServerCertThumbprint 850A278CE7364C9E88B72F65FD38304F1137E61E `
+    -FindType FindByThumbprint -FindValue 850A278CE7364C9E88B72F65FD38304F1137E61E `
+    -StoreLocation CurrentUser -StoreName My
